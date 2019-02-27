8703afe6c4c3c939c30cc6c0bd75eac4df09c69a
Novsky <djnovsky@gmail.com>
2019-02-28 00:10:54 +0200

    28.02

--- JS4kds/js-test.js
+++ JS4kds/js-test.js
@@ -1,9 +1,9 @@
 let times = 0;
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let string = "";
-while(times<6){
+while(times<9){
     let random = Math.floor(Math.random()*alphabet.length);
     string += alphabet[random]
     times++;
 }
-console.log("Random word today is - " + string)
+console.log("Random word today is - " + string.toLowerCase())
